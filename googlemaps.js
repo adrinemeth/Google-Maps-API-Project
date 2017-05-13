bf34a808fbf45812af941924d0f979cf63162178
Adri <adri.nemeth92@gmail.com>
2017-05-13 14:39:15 +0200

    added markers to the map + implemented MVC

--- googlemaps.js
+++ googlemaps.js
@@ -1,16 +1,52 @@
 var model = {
-    amsterdam: {
-        lat: 52.364,
-        lng: 4.864
-    },
-    map: function () {
-        return new google.maps.Map(document.getElementById('map'), {
-            zoom: 11,
-            center: model.amsterdam
-        })
+    markers: [
+        {
+            position: { lat: 52.371700, lng: 4.914827 },
+            name: "NATIONAL MARITIME MUSEUM"
+        },
+        {
+            position: { lat: 52.374206, lng: 4.912335 },
+            name: "SCIENCE CENTER NEMO"
+        },
+        {
+            position: { lat: 52.366791, lng: 4.892730 },
+            name: "STINS FLOWER MARKET"
+        },
+        {
+            position: { lat: 52.379144, lng: 4.900132 },
+            name: "AMSTERDAM CENTRAAL"
+        },
+        {
+            position: { lat: 52.365378, lng: 4.959510 },
+            name: "CAMPING ZEEBURG AMSTERDAM"
+        }
+    ]
+}
+var controller = {
+    getMarkers: function () {
+        return model.markers;
     }
 }
-
-function initMap() {
-    model.map();
-}
+var mapView = {
+    init: function () {
+        var markers = controller.getMarkers();
+        var amsterdam = { lat: 52.374685, lng: 4.899757 };
+        var map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 12,
+            center: amsterdam
+        });
+        markerView.render(markers, map);
+    }
+};
+var markerView = {
+    render: function (array, baseMap) {
+        array.forEach(function (item) {
+            console.log(item);
+            var marker = new google.maps.Marker({
+                position: item.position,
+                map: baseMap,
+                title: item.name
+            });
+        });
+    }
+};
