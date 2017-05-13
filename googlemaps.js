521ef2d90af7c40bc1b9480ebdac49a3a36afb52
Adri <adri.nemeth92@gmail.com>
2017-05-13 16:04:13 +0200

    added search field to filter markers

--- googlemaps.js
+++ googlemaps.js
@@ -2,15 +2,15 @@ var model = {
     markers: [
         {
             position: { lat: 52.371700, lng: 4.914827 },
-            name: "NATIONAL MARITIME MUSEUM"
+            name: "Het Scheepvaartmuseum"
         },
         {
             position: { lat: 52.374206, lng: 4.912335 },
-            name: "SCIENCE CENTER NEMO"
+            name: "NEMO Science Museum"
         },
         {
             position: { lat: 52.366791, lng: 4.892730 },
-            name: "STINS FLOWER MARKET"
+            name: "Bloemenmarkt"
         },
         {
             position: { lat: 52.379144, lng: 4.900132 },
@@ -18,15 +18,15 @@ var model = {
         },
         {
             position: { lat: 52.365378, lng: 4.959510 },
-            name: "CAMPING ZEEBURG AMSTERDAM"
+            name: "Zeeburg Camping"
         }
     ]
-}
+};
 var controller = {
     getMarkers: function () {
         return model.markers;
     }
-}
+};
 var mapView = {
     init: function () {
         var markers = controller.getMarkers();
@@ -42,7 +42,6 @@ var mapView = {
 var markerView = {
     render: function (array, baseMap) {
         array.forEach(function (item) {
-            console.log(item);
             var marker = new google.maps.Marker({
                 position: item.position,
                 map: baseMap,
@@ -52,31 +51,48 @@ var markerView = {
     }
 };
 var markerList = {
-    init: function(baseMap) {
-      markerList.render(baseMap);
-      markerList.update();
+    init: function (baseMap) {
+        markerList.render(baseMap);
+        markerList.update();
     },
-    render: function(baseMap) {
-      var markers = controller.getMarkers();
-      this.markerListElem = document.getElementById("marker-list");
-      var length = markers.length;
-      for (var i = 1; i < length; i++) {
-        // Create the list item:
-        var marker = markers[i];
-        var item = document.createElement("a");
-        var listElem = document.createElement("li");
-        // Set its contents:
-        item.textContent = marker.name;
-        function getPosition(x) {
-          return function() {
-            baseMap.setZoom(15);
-            baseMap.setCenter(x);
-          };
+    render: function (baseMap) {
+        var markers = controller.getMarkers();
+        this.markerListElem = document.getElementById("marker-list");
+        var length = markers.length;
+        for (var i = 1; i < length; i++) {
+            // Create the list item:
+            var marker = markers[i];
+            var item = document.createElement("a");
+            var listElem = document.createElement("li");
+            // Set its contents:
+            item.textContent = marker.name;
+            function getPosition(x) {
+                return function () {
+                    baseMap.setZoom(15);
+                    baseMap.setCenter(x);
+                };
+            }
+            item.addEventListener("click", getPosition(marker.position));
+            // Add it to the list:
+            listElem.appendChild(item);
+            this.markerListElem.appendChild(listElem);
         }
-        item.addEventListener("click", getPosition(marker.position));
-        // Add it to the list:
-        listElem.appendChild(item);
-        this.markerListElem.appendChild(listElem);
-      }
+    },
+    update: function () {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("search");
+        input.addEventListener("keyup", function () {
+            filter = this.value.toUpperCase();
+            ul = document.getElementById("marker-list");
+            li = ul.getElementsByTagName("li");
+            for (i = 0; i < li.length; i++) {
+                a = li[i].getElementsByTagName("a")[0];
+                if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    li[i].style.display = "";
+                } else {
+                    li[i].style.display = "none";
+                }
+            }
+        });
     }
 };
